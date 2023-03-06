7842171acf689822b07e2b443a7a4a8c88a4fd83
Salamana2009 <126754289+Salamana2009@users.noreply.github.com>
2023-03-06 22:03:53 +0500

    Add files via upload

---
+++ zizaz/idle.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+a = int(input("Birinchi sonni kirit:"))
+b = int(input("Ikkinchi sonni kirit:"))
+c = a ,+ b
+print("Javobi" , c)
+
---
+++ zizaz/key.py
@@ -0,0 +1,5 @@
+print("Hello world")
+a= int(input("Birinchi sonni kirit:"))
+b= int(input("Ikkinchi sonni kirit:"))
+c= a*b
+print("Javob" , c)
