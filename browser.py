6e2a9251ef701750562187e5bf9284d5a6da14b4
Salamana2009 <126754289+Salamana2009@users.noreply.github.com>
2023-05-05 20:06:31 +0500

    Browser.py

---
+++ browser.py
@@ -0,0 +1,57 @@
+
+#with open('input.txt', 'r') as input_file:
+#    # Read the numbers from the file and split them into a list
+#    numbers = input_file.read().split()
+
+#even_sum = 0
+
+#for number in numbers:
+#    # Convert the number from a string to an integer
+#    number = int(number)
+#    # Check if the number is even
+#    if number % 2 == 0:
+#        # If it's even, add it to the sum
+#        even_sum += number
+
+#with open('output.txt', 'w') as output_file:
+#    # Create a list of the even numbers as strings
+#    even_numbers = [str(number) for number in numbers if int(number) % 2 == 0]
+#    # Join the even numbers together with plus signs
+#    even_numbers_string = ' + '.join(even_numbers)
+#    # Write the equation to the file
+#    output_file.write(even_numbers_string + ' = ' + str(even_sum))
+
+
+############################################################################################################################
+
+
+############################################################################################################################
+
+#with open('INPUT.TXT', 'r') as input_file:
+#    a = int(input_file.readline().strip())
+#    b = int(input_file.readline().strip())
+
+#sum_ab = a + b
+
+
+#with open('OUTPUT.TXT', 'w') as output_file:
+#    output_file.write(str(sum_ab))
+
+############################################################################################################################
+
+############################################################################################################################
+
+#with open('input.txt', 'r') as input_file:
+    
+#    numbers = input_file.readline().strip().split()
+
+#    results = []
+
+#    for num in numbers:
+#        result = int(num) ** int(num)
+#        results.append(str(result))
+
+  
+#    with open('output.txt', 'w') as output_file:
+     
+#        output_file.write(' '.join(results))
